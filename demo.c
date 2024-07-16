357bb1d13b99c51c5ba4a7cd79f14fdb55c17480
suryaprakash08100 <147241454+suryaprakash08100@users.noreply.github.com>
2024-07-16 09:39:53 +0530

    Create demo.c

---
+++ demo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+   printf("Enter a number: ");
+   scanf("%d",&a);
+if(a%2==0)
+  printf("%d is a even number",a);
+else
+  printf("%d is a odd number",a);
+}
